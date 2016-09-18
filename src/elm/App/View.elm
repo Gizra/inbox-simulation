5c65180b9072c388ddd53566c01f6bfa9a5058b2
Amitaibu <amitai@gizra.com>
2016-09-18 12:02:06 +0300

    Add semantic UI

--- src/elm/App/View.elm
+++ src/elm/App/View.elm
@@ -1,15 +1,12 @@
 module App.View exposing (..)
 
-import Exts.RemoteData exposing (RemoteData(..), WebData)
 import Html exposing (..)
 import Html.Attributes exposing (class, classList, href, src, style, target)
 import Html.App as Html
 import Html.Events exposing (onClick)
 import App.Model exposing (..)
 import App.Update exposing (..)
-import User.Model exposing (..)
-import Pages.Login.View exposing (..)
-import Pages.MyAccount.View exposing (..)
+import Pages.Counter.View exposing (..)
 import Pages.PageNotFound.View exposing (..)
 
 
@@ -21,8 +18,7 @@ view model =
             , viewMainContent model
             , pre [ class "ui padded secondary segment" ]
                 [ div [] [ text <| "activePage: " ++ toString model.activePage ]
-                , div [] [ text <| "pageLogin: " ++ toString model.pageLogin ]
-                , div [] [ text <| "user: " ++ toString model.user ]
+                , div [] [ text <| "pageCounter: " ++ toString model.pageCounter ]
                 ]
             ]
         , viewFooter
@@ -31,45 +27,17 @@ view model =
 
 viewHeader : Model -> Html Msg
 viewHeader model =
-    let
-        navbar =
-            case model.user of
-                Success _ ->
-                    navbarAuthenticated
-
-                _ ->
-                    navbarAnonymous
-    in
-        div [ class "ui secondary pointing menu" ] (navbar model)
+    div [ class "ui secondary pointing menu" ] (navbarAnonymous model)
 
 
 navbarAnonymous : Model -> List (Html Msg)
 navbarAnonymous model =
     [ a
-        [ classByPage Login model.activePage
-        , onClick <| SetActivePage Login
+        [ classByPage Counter model.activePage
+        , onClick <| SetActivePage Counter
         ]
-        [ text "Login" ]
+        [ text "Counter" ]
     , viewPageNotFoundItem model.activePage
-    ]
-
-
-navbarAuthenticated : Model -> List (Html Msg)
-navbarAuthenticated model =
-    [ a
-        [ classByPage MyAccount model.activePage
-        , onClick <| SetActivePage MyAccount
-        ]
-        [ text "My Account" ]
-    , viewPageNotFoundItem model.activePage
-    , div [ class "right menu" ]
-        [ viewAvatar model.user
-        , a
-            [ class "ui item"
-            , onClick <| Logout
-            ]
-            [ text "Logout" ]
-        ]
     ]
 
 
@@ -82,36 +50,11 @@ viewPageNotFoundItem activePage =
         [ text "404 page" ]
 
 
-viewAvatar : WebData User -> Html Msg
-viewAvatar user =
-    case user of
-        Success user' ->
-            a
-                [ onClick <| SetActivePage MyAccount
-                , class "ui item"
-                ]
-                [ img
-                    [ class "ui avatar image"
-                    , src user'.avatarUrl
-                    ]
-                    []
-                ]
-
-        _ ->
-            div [] []
-
-
 viewMainContent : Model -> Html Msg
 viewMainContent model =
     case model.activePage of
-        AccessDenied ->
-            div [] [ text "Access denied" ]
-
-        Login ->
-            Html.map PageLogin (Pages.Login.View.view model.user model.pageLogin)
-
-        MyAccount ->
-            Pages.MyAccount.View.view model.user
+        Counter ->
+            Html.map PageCounter (Pages.Counter.View.view model.pageCounter)
 
         PageNotFound ->
             -- We don't need to pass any cmds, so we can call the view directly
