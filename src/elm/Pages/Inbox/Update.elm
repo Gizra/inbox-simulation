ba43ba3684a3dc0ace2e6de08031cd8701115287
Amitaibu <amitai@gizra.com>
2016-09-18 16:17:49 +0300

    Allow unselecting email

--- src/elm/Pages/Inbox/Update.elm
+++ src/elm/Pages/Inbox/Update.elm
@@ -17,4 +17,22 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update action model =
     case action of
         SetSelectedEmail name ->
-            { model | selectedEmail = name } ! []
+            -- If same email is selected, we un-select the emails.
+            let
+                selectedEmail =
+                    case model.selectedEmail of
+                        Nothing ->
+                            name
+
+                        Just val ->
+                            case name of
+                                Nothing ->
+                                    Just val
+
+                                Just val' ->
+                                    if val == val' then
+                                        Nothing
+                                    else
+                                        Just val'
+            in
+                { model | selectedEmail = selectedEmail } ! []
