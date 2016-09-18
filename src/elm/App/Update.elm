126dcd5802b4693eabcdd3faa696b9a2cbf1b249
Amitaibu <amitai@gizra.com>
2016-09-18 11:56:20 +0300

    More work

--- src/elm/App/Update.elm
+++ src/elm/App/Update.elm
@@ -1,8 +1,6 @@
 module App.Update exposing (init, update, Msg(..))
 
 import App.Model exposing (..)
-import Exts.RemoteData exposing (RemoteData(..), WebData)
-import User.Model exposing (..)
 import Pages.Counter.Update exposing (Msg)
 
 
@@ -21,24 +19,13 @@ update msg model =
     case msg of
         PageCounter msg ->
             let
-                ( val, cmds, user ) =
+                ( val, cmds ) =
                     Pages.Counter.Update.update msg model.pageCounter
 
                 model' =
                     { model | pageCounter = val }
-
-                model'' =
-                    case user of
-                        -- If user was successfuly fetched, reditect to my
-                        -- account page.
-                        Success _ ->
-                            update (SetActivePage MyAccount) model'
-                                |> fst
-
-                        _ ->
-                            model'
             in
-                ( model'', Cmd.map PageLogin cmds )
+                ( model', Cmd.map PageCounter cmds )
 
         SetActivePage page ->
-            { model | activePage = setActivePageAccess model.user page } ! []
+            { model | activePage = page } ! []
