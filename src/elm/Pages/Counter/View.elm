7f26baf2214efaa781b0823606782703fd7d00f6
Amitaibu <amitai@gizra.com>
2016-09-18 12:19:58 +0300

    Static to dynamic

--- src/elm/Pages/Counter/View.elm
+++ src/elm/Pages/Counter/View.elm
@@ -1,7 +1,7 @@
 module Pages.Counter.View exposing (view)
 
 import Html exposing (..)
-import Html.Attributes exposing (style)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Pages.Counter.Model exposing (..)
 import Pages.Counter.Update exposing (..)
@@ -10,18 +10,32 @@ import Pages.Counter.Update exposing (..)
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [ countStyle ] [ text (toString model) ]
-        , button [ onClick Increment ] [ text "+" ]
-        ]
-
-
-countStyle : Attribute msg
-countStyle =
-    style
-        [ ( "font-size", "20px" )
-        , ( "font-family", "monospace" )
-        , ( "display", "inline-block" )
-        , ( "width", "50px" )
-        , ( "text-align", "center" )
+        [ div [ class "container" ]
+            [ div [ class "ui inverted menu top__menu" ]
+                [ a [ class "ui item microsoft_logo" ]
+                    [ i [ class "grid layout icon" ]
+                        []
+                    ]
+                , div [ class "header item" ]
+                    [ text "Outlook Mail" ]
+                , div [ class "right menu" ]
+                    [ a [ class "ui item" ]
+                        [ i [ class "skype icon" ]
+                            []
+                        ]
+                    , a [ class "ui item" ]
+                        [ i [ class "alarm icon" ]
+                            []
+                        ]
+                    , a [ class "ui item" ]
+                        [ i [ class "setting icon" ]
+                            []
+                        ]
+                    , a [ class "ui item" ]
+                        [ i [ class "help icon" ]
+                            []
+                        ]
+                    ]
+                ]
+            ]
         ]
